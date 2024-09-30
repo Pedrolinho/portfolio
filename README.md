83eea9a438fd439345a71d88b126dc984a29d5d3
Pedro André <pedroandre.lcc@gmail.com>
2024-09-30 08:26:14 -0300

    v0.3 - Menu de Navegação CSS Pronto; Cards de Conteúdo; Modal e Conteúdo;

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Portfólio
 Este repositório contém os arquivos do meu portfólio pessoal, desenvolvido para exibir meus projetos e habilidades como programador e designer 3D.
+
+Projeto feito para o Processo Seletivo da Apple Developer Academy PUCPR.
+
+Acesse já o link do meu portfólio: [https://pedrolinho.github.io/portfolio/](https://pedrolinho.github.io/portfolio/).
+
--- script.js
+++ script.js
@@ -17,7 +17,67 @@ navItems.forEach(item => {
         // Pega o alvo da seção clicada
         const target = item.getAttribute('data-target');
 
-        // Adiciona a classe "active-section" à seção correspondente
+        // Adiciona a classe "active" à seção correspondente
         document.getElementById(target).classList.add('active');
     });
 });
+
+// Seleciona os elementos do modal e os botões
+const modal = document.querySelector('#modal');
+const fade = document.querySelector('#fade');
+const closeModalButton = document.querySelector('#modal-close');
+const modalSections = document.querySelectorAll('.modal-section');
+
+const modalHeaderProjetos = document.querySelector('#modal-header-projetos')
+
+function openModal(sectionId, headerId) {
+    showDescricao(sectionId);
+    showHeader(headerId);
+}
+function showHeader (headerId) {
+    headerId.classList.remove('hide');
+}
+
+// Função para abrir o modal e mostrar a seção correta
+function showDescricao(sectionId) {
+    // Mostra a seção específica
+    const sectionToShow = document.getElementById(sectionId);
+    sectionToShow.classList.remove('modal-hide');
+
+    // Exibe o modal e o fade
+    modal.classList.remove('hide');
+    fade.classList.remove('hide');
+}
+
+// Função para fechar o modal
+function closeModal() {
+    // Esconde todas as seções do modal
+    modalSections.forEach(section => section.classList.add('modal-hide'));
+
+    modalHeaderProjetos.classList.add('hide');
+    modal.classList.add('hide');
+    fade.classList.add('hide');
+}
+
+// Adiciona evento de clique para cada botão que abre o modal
+document.getElementById('btn-mapeamento-sistematico').addEventListener('click', () => {
+    openModal('mapeamento-sistematico', modalHeaderProjetos);
+});
+document.getElementById('btn-ufpr-baja').addEventListener('click', () => {
+    openModal('ufpr-baja', modalHeaderProjetos);
+});
+document.getElementById('btn-paft-puc').addEventListener('click', () => {
+    openModal('paft-puc', modalHeaderProjetos);
+});
+document.getElementById('btn-cientista-sincero').addEventListener('click', () => {
+    openModal('cientista-sincero', modalHeaderProjetos);
+});
+document.getElementById('btn-jokenpog').addEventListener('click', () => {
+    openModal('jokenpog', modalHeaderProjetos);
+});
+
+// Evento para fechar o modal ao clicar no botão de fechar
+closeModalButton.addEventListener('click', closeModal);
+
+// Evento para fechar o modal ao clicar fora (no fade)
+fade.addEventListener('click', closeModal);
